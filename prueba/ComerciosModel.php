62b9de7adca1a7f125ad20f5897f002a2cf4bb8f
Aaron <aaron.jimenez.teixeira@ikasle.egibide.org>
2025-10-17 10:33:23 +0200

    Add files via upload

---
+++ prueba/ComerciosModel.php
@@ -0,0 +1,73 @@
+<?php
+require_once __DIR__ . '/Database.php';
+
+class ComerciosModel {
+    
+    public static function getAll() {
+        $dbh = Database::getConnection();
+        $stmt = $dbh->prepare("SELECT *
+                                FROM anuncios");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+    public static function create($datos,$imagenes = []) {
+        $dbh = Database::getConnection();
+        $stmt= $dbh->prepare("INSERT INTO anuncios(titulo,descripcion,direccion,precio,id_usuario,id_categoria,estado)
+                            values (:titulo,:descripcion,:direccion,:precio,:id_usuario,:id_categoria,true)"
+        );
+        $data = array(
+            "titulo" => $datos[0],
+            "descripcion" => $datos[1],
+            "direccion" => $datos[2],
+            "precio" => $datos[3],
+            "id_usuario" => $datos[4],
+            "id_categoria" => $datos[5]
+        );
+        if($stmt->execute($data)){
+            if(!empty($imagenes))
+                ImagenesModel::create($dbh,$imagenes);
+        }else{
+            throw new Exception("No se pudo añadir el anuncio a la base de datos");
+        }    
+    }
+    
+    public static function deleteById($id) {
+        $dbh = Database::getConnection();
+        $stmt= $dbh->prepare("DELETE FROM anuncios
+                                WHERE id = :id"
+        );
+        $data = array('id' => $id);
+        if (!$stmt->execute($data)):
+            throw new Exception("No se pudo borrar el anuncio a la base de datos");
+        endif;
+    }
+    
+    public static function deleteAll($id_usuario) {
+        $dbh = Database::getConnection();
+        $stmt= $dbh->prepare("DELETE FROM anuncios
+                                WHERE id_usuario = :id_usuario");
+        if(!$stmt->execute([
+            'id_usuario' => $id_usuario
+        ])):
+            throw new Exception("No se pudo borrar ningún anuncio de la base de datos");
+        endif;
+    }
+    
+    public static function edit($datos) {
+        $dbh = Database::getConnection();
+        $stmt= $dbh->prepare("UPDATE anuncios
+                            SET titulo = :titulo, descripcion = :descripcion, direccion = :direccion, precio = :precio, id_categoria = :id_categoria
+                            WHERE id = :id"
+        );
+        $data = array(
+            "id" => $datos[0],
+            "titulo" => $datos[1],
+            "descripcion" => $datos[2],
+            "direccion" => $datos[3],
+            "precio" => $datos[4],
+            "id_categoria" => $datos[5]
+        );
+        if(!$stmt->execute($data)) throw new Exception("No se pudo editar el anuncio");
+    }
+}
---
+++ prueba/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'comercios_vitoria');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('MEDIA_FOLDER', 'media/anuncios/');

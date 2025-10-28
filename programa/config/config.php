50b675622b93ce700552e10dae3becdf32b9841e
imanolmanero <imanol.manero@ikasle.egibide.org>
2025-10-28 09:19:52 +0100

    no funciona para los demas

--- programa/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Configuración de la base de datos
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'comercios_vitoria');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('MEDIA_FOLDER', 'media/anuncios/');
